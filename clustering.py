acf30dc86412e056e149bf5a2be936ba6daf8529
istabraq <istabraqruhaily@gmail.com>
2021-07-06 20:25:39 +0400

    initial commit

---
+++ clustering.py
@@ -0,0 +1,13 @@
+# importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+X= -2 * np.random.rand(100,2)
+X1 = 1 + 2 * np.random.rand(50,2)
+X[50:100, :] = X1
+plt.scatter(X[ : , 0], X[ :, 1], s = 50, c = 'b')
+plt.show()
+
+
